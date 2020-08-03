esaueng/factory-assistant-core

commit 4c49dcfa4a1fcc8e3541c5449db6543a5cd8d9ea

Fix pre-commit hook issue on Windows (#31648)
* Make shebang compatable with Windows

* Prehook test

* Undo prehook test

script/run-in-env.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/usr/bin/env sh -eu
 
 # Activate pyenv and virtualenv if present, then run the specified command
 

commit 8913f8f09a480c984965a605b54772ef570cc6a9

Fix run-in-env.sh sh options (#38520)
Shebang takes only one arg, regression in
4c49dcfa4a1fcc8e3541c5449db6543a5cd8d9ea

script/run-in-env.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh -eu
+#!/usr/bin/env sh
+set -eu
 
 # Activate pyenv and virtualenv if present, then run the specified command
 