94d5fdcf8fb998c3d54a321aa0aaba3d7da2924f
Robert Hillis <tkdrob4390@yahoo.com>
2022-09-26 18:22:58 -0400

    Remove issue from Radarr (#79127)

--- homeassistant/components/radarr/__init__.py
+++ homeassistant/components/radarr/__init__.py
@@ -50,15 +50,6 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
                 severity=IssueSeverity.WARNING,
                 REDACTED_VALUE"deprecated_yaml",
             )
-            async_create_issue(
-                hass,
-                DOMAIN,
-                "removed_attributes",
-                breaks_in_ha_version="2022.10.0",
-                is_fixable=False,
-                severity=IssueSeverity.WARNING,
-                REDACTED_VALUE"removed_attributes",
-            )
 
     return True
 
