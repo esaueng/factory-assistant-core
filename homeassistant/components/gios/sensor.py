96d2622c2ea29162aeef912d236becbee32259f0
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-03-06 18:12:19 +0100

    Bump `gios` library to version 3.1.0 (#89044)

--- homeassistant/components/gios/sensor.py
+++ homeassistant/components/gios/sensor.py
@@ -60,6 +60,10 @@ SENSOR_TYPES: tuple[GiosSensorEntityDescription, ...] = (
         key=ATTR_AQI,
         name="AQI",
         value=None,
+        icon="mdi:air-filter",
+        device_class=SensorDeviceClass.ENUM,
+        options=["very_bad", "bad", "sufficient", "moderate", "good", "very_good"],
+        REDACTED_VALUE"aqi",
     ),
     GiosSensorEntityDescription(
         key=ATTR_C6H6,
