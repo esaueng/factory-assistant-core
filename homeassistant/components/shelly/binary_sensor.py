a59fff1182639811f92acbbaa5bb6bc6f7439ca1
Maciej Bieniek <bieniu@users.noreply.github.com>
2023-03-15 18:52:42 +0100

    Use `SensorDeviceClass.ENUM` and add state attributes translations in Shelly integration (#89660)

--- homeassistant/components/shelly/binary_sensor.py
+++ homeassistant/components/shelly/binary_sensor.py
@@ -91,6 +91,7 @@ SENSORS: Final = {
         key="sensor|gas",
         name="Gas",
         device_class=BinarySensorDeviceClass.GAS,
+        REDACTED_VALUE"gas",
         value=lambda value: value in ["mild", "heavy"],
         extra_state_attributes=lambda block: {"detected": block.gas},
     ),
