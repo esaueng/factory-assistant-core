c3696e8117a2e839d6147b2226f97bb5706e1325
G Johansson <goran.johansson@shiftit.se>
2023-05-31 03:22:11 +0200

    Translate entity names for Yale Smart Living (#93560)

--- homeassistant/components/yale_smart_alarm/button.py
+++ homeassistant/components/yale_smart_alarm/button.py
@@ -15,7 +15,7 @@ from .entity import YaleAlarmEntity
 BUTTON_TYPES = (
     ButtonEntityDescription(
         key="panic",
-        name="Panic button",
+        REDACTED_VALUE"panic",
         icon="mdi:alarm-light",
     ),
 )
