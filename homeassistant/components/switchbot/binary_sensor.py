eb005e815afea3cf99ee22c3a7e86026ac61eeaf
Patrick ZAJDA <patrick@zajda.fr>
2023-07-07 19:38:43 +0200

    Set some Switchbot entity names to none (#90846)

--- homeassistant/components/switchbot/binary_sensor.py
+++ homeassistant/components/switchbot/binary_sensor.py
@@ -25,12 +25,12 @@ BINARY_SENSOR_TYPES: dict[str, BinarySensorEntityDescription] = {
     ),
     "motion_detected": BinarySensorEntityDescription(
         key="pir_state",
-        REDACTED_VALUE"motion",
+        name=None,
         device_class=BinarySensorDeviceClass.MOTION,
     ),
     "contact_open": BinarySensorEntityDescription(
         key="contact_open",
-        REDACTED_VALUE"door_open",
+        name=None,
         device_class=BinarySensorDeviceClass.DOOR,
     ),
     "contact_timeout": BinarySensorEntityDescription(
@@ -46,7 +46,7 @@ BINARY_SENSOR_TYPES: dict[str, BinarySensorEntityDescription] = {
     ),
     "door_open": BinarySensorEntityDescription(
         key="door_status",
-        REDACTED_VALUE"door_open",
+        name=None,
         device_class=BinarySensorDeviceClass.DOOR,
     ),
     "unclosed_alarm": BinarySensorEntityDescription(
