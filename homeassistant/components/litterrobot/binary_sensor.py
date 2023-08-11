90e7af75d48177ecccf8526ba036b60b2a642cc0
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-08-11 13:13:04 +0200

    Update mypy to 1.5.0 (#98179)

--- homeassistant/components/litterrobot/binary_sensor.py
+++ homeassistant/components/litterrobot/binary_sensor.py
@@ -48,7 +48,7 @@ class LitterRobotBinarySensorEntity(LitterRobotEntity[_RobotT], BinarySensorEnti
 
 
 BINARY_SENSOR_MAP: dict[type[Robot], tuple[RobotBinarySensorEntityDescription, ...]] = {
-    LitterRobot: (
+    LitterRobot: (  # type: ignore[type-abstract]  # only used for isinstance check
         RobotBinarySensorEntityDescription[LitterRobot](
             key="sleeping",
             REDACTED_VALUE"sleeping",
@@ -66,7 +66,7 @@ BINARY_SENSOR_MAP: dict[type[Robot], tuple[RobotBinarySensorEntityDescription, .
             is_on_fn=lambda robot: robot.sleep_mode_enabled,
         ),
     ),
-    Robot: (
+    Robot: (  # type: ignore[type-abstract]  # only used for isinstance check
         RobotBinarySensorEntityDescription[Robot](
             key="power_status",
             REDACTED_VALUE"power_status",
