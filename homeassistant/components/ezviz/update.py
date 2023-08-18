02f1e531979cf2fded324c4c8f76f5ef64b46e76
Robert Resch <robert@resch.dev>
2023-08-18 13:40:35 +0200

    Name unnamed update entities by their device class (#98579)

--- homeassistant/components/ezviz/update.py
+++ homeassistant/components/ezviz/update.py
@@ -24,7 +24,6 @@ PARALLEL_UPDATES = 1
 
 UPDATE_ENTITY_TYPES = UpdateEntityDescription(
     key="version",
-    REDACTED_VALUE"firmware",
     device_class=UpdateDeviceClass.FIRMWARE,
 )
 
--- homeassistant/components/litterrobot/update.py
+++ homeassistant/components/litterrobot/update.py
@@ -24,7 +24,6 @@ SCAN_INTERVAL = timedelta(days=1)
 
 FIRMWARE_UPDATE_ENTITY = UpdateEntityDescription(
     key="firmware",
-    REDACTED_VALUE"firmware",
     device_class=UpdateDeviceClass.FIRMWARE,
 )
 
