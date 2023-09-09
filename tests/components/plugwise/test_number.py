cfe7d269d447e11bbd332cc7873270969b6cfd36
Bouwe Westerdijk <11290930+bouwew@users.noreply.github.com>
2023-09-09 23:50:26 +0200

    Bump plugwise to v0.32.2 (#99973)
    * Bump plugwise to v0.32.2
    
    * Adapt number.py to the backend updates
    
    * Update related test-cases
    
    * Update plugwise test-fixtures
    
    * Update test_diagnostics.py

--- tests/components/plugwise/test_number.py
+++ tests/components/plugwise/test_number.py
@@ -38,7 +38,7 @@ async def test_anna_max_boiler_temp_change(
 
     assert mock_smile_anna.set_number_setpoint.call_count == 1
     mock_smile_anna.set_number_setpoint.assert_called_with(
-        "maximum_boiler_temperature", 65.0
+        "maximum_boiler_temperature", "REDACTED_VALUE", 65.0
     )
 
 
@@ -67,5 +67,5 @@ async def test_adam_dhw_setpoint_change(
 
     assert mock_smile_adam_2.set_number_setpoint.call_count == 1
     mock_smile_adam_2.set_number_setpoint.assert_called_with(
-        "max_dhw_temperature", 55.0
+        "max_dhw_temperature", "REDACTED_VALUE", 55.0
     )
