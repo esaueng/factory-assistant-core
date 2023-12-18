e99432e4481b42b00c868dadd336a2b3eccd27ce
starkillerOG <starkiller.og@gmail.com>
2023-12-18 11:13:20 +0100

    Reolink change ir to switch (#105916)
    * Change IR from light to switch
    
    * Remove old entity
    
    * Add test
    
    * Apply suggestions from code review
    
    Co-authored-by: Jan-Philipp Benecke <github@bnck.me>
    
    ---------
    
    Co-authored-by: Jan-Philipp Benecke <github@bnck.me>

--- homeassistant/components/reolink/switch.py
+++ homeassistant/components/reolink/switch.py
@@ -49,6 +49,16 @@ class ReolinkNVRSwitchEntityDescription(
 
 SWITCH_ENTITIES = (
     ReolinkSwitchEntityDescription(
+        key="ir_lights",
+        cmd_key="GetIrLights",
+        REDACTED_VALUE"ir_lights",
+        icon="mdi:led-off",
+        entity_category=EntityCategory.CONFIG,
+        supported=lambda api, ch: api.supported(ch, "ir_lights"),
+        value=lambda api, ch: api.ir_enabled(ch),
+        method=lambda api, ch, value: api.set_ir_lights(ch, value),
+    ),
+    ReolinkSwitchEntityDescription(
         key="record_audio",
         cmd_key="GetEnc",
         REDACTED_VALUE"record_audio",
