b9302d3a7e60c33dea61200410806588caf94268
Franck Nijhof <git@frenck.dev>
2024-01-11 23:25:33 +0100

    Fix Tailwind cover stuck in closing state (#107827)

--- homeassistant/components/tailwind/cover.py
+++ homeassistant/components/tailwind/cover.py
@@ -121,5 +121,6 @@ class TailwindDoorCoverEntity(TailwindDoorEntity, CoverEntity):
                 translation_domain=DOMAIN,
                 REDACTED_VALUE"communication_error",
             ) from exc
-        self._attr_is_closing = False
+        finally:
+            self._attr_is_closing = False
         await self.coordinator.async_request_refresh()
