7b137a472f15aa4e3183b4583ae200301b54e1ef
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-01-21 13:26:28 +0100

    Add icon translations for lamarzocco (#108569)
    icon translations

--- homeassistant/components/lamarzocco/button.py
+++ homeassistant/components/lamarzocco/button.py
@@ -29,7 +29,6 @@ ENTITIES: tuple[LaMarzoccoButtonEntityDescription, ...] = (
     LaMarzoccoButtonEntityDescription(
         key="start_backflush",
         REDACTED_VALUE"start_backflush",
-        icon="mdi:water-sync",
         press_fn=lambda lm: lm.start_backflush(),
     ),
 )
