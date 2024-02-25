bd8ad2d77d5d83bce2bb8bcb80ef54b9c1fc44e5
Joost Lekkerkerker <joostlek@outlook.com>
2024-02-25 14:03:07 +0100

    Add icon translations to Coolmaster (#111359)

--- homeassistant/components/coolmaster/sensor.py
+++ homeassistant/components/coolmaster/sensor.py
@@ -32,7 +32,6 @@ class CoolmasterCleanFilter(CoolmasterEntity, SensorEntity):
         key="error_code",
         REDACTED_VALUE"error_code",
         entity_category=EntityCategory.DIAGNOSTIC,
-        icon="mdi:alert",
     )
 
     @property
