76e4fc274a1acb172136861e09862bcd159303de
Joost Lekkerkerker <joostlek@outlook.com>
2024-03-01 13:42:42 +0100

    Add icon translations to Lyric (#111902)
    Use default icon for Lyric

--- homeassistant/components/lyric/sensor.py
+++ homeassistant/components/lyric/sensor.py
@@ -105,7 +105,6 @@ DEVICE_SENSORS: list[LyricSensorEntityDescription] = [
     LyricSensorEntityDescription(
         key="setpoint_status",
         REDACTED_VALUE"setpoint_status",
-        icon="mdi:thermostat",
         value_fn=lambda device: get_setpoint_status(
             device.changeableValues.thermostatSetpointStatus,
             device.changeableValues.nextPeriodTime,
