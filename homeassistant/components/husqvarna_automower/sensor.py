c35fec24e86999a659a594f684f719edc4073aae
Joost Lekkerkerker <joostlek@outlook.com>
2024-03-10 21:55:31 +0100

    Add icon translations to Husqvarna automower (#111727)
    * Add icon translations to Husqvarna automower
    
    * fix

--- homeassistant/components/husqvarna_automower/sensor.py
+++ homeassistant/components/husqvarna_automower/sensor.py
@@ -105,7 +105,6 @@ SENSOR_TYPES: tuple[AutomowerSensorEntityDescription, ...] = (
     AutomowerSensorEntityDescription(
         key="number_of_charging_cycles",
         REDACTED_VALUE"number_of_charging_cycles",
-        icon="mdi:battery-sync-outline",
         entity_category=EntityCategory.DIAGNOSTIC,
         state_class=SensorStateClass.TOTAL,
         value_fn=lambda data: data.statistics.number_of_charging_cycles,
@@ -113,7 +112,6 @@ SENSOR_TYPES: tuple[AutomowerSensorEntityDescription, ...] = (
     AutomowerSensorEntityDescription(
         key="number_of_collisions",
         REDACTED_VALUE"number_of_collisions",
-        icon="mdi:counter",
         entity_category=EntityCategory.DIAGNOSTIC,
         state_class=SensorStateClass.TOTAL,
         value_fn=lambda data: data.statistics.number_of_collisions,
