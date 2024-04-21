f1bd5c797298cf53c0f61d49dfbb4785c5d79898
Jan Bouwhuis <jbouwh@users.noreply.github.com>
2024-04-21 08:54:23 +0200

    Improve service validation exception test and translation key (#115843)
    * Small improvement to service validation exception test and translation key
    
    * Apply suggestions from code review
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>
    
    * Refactor string assertion
    
    ---------
    
    Co-authored-by: Martin Hjelmare <marhje52@gmail.com>

--- homeassistant/core.py
+++ homeassistant/core.py
@@ -2589,7 +2589,7 @@ class ServiceRegistry:
             if handler.supports_response is SupportsResponse.NONE:
                 raise ServiceValidationError(
                     translation_domain=DOMAIN,
-                    REDACTED_VALUE"service_does_not_supports_reponse",
+                    REDACTED_VALUE"service_does_not_support_response",
                     translation_placeholders={
                         "return_response": "return_response=True"
                     },
