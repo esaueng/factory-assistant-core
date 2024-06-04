ab177b29ddce67deaa87526dbf9a461f2aabd1a4
epenet <6771947+epenet@users.noreply.github.com>
2024-06-04 13:38:32 +0200

    Move entity_registry_enabled_by_default to decorator [q-z] (#118793)

--- tests/components/solaredge/test_coordinator.py
+++ tests/components/solaredge/test_coordinator.py
@@ -21,7 +21,7 @@ API_KEY = "REDACTED_VALUE"
 
 
 @pytest.fixture(autouse=True)
-def enable_all_entities(entity_registry_enabled_by_default):
+def enable_all_entities(entity_registry_enabled_by_default: None) -> None:
     """Make sure all entities are enabled."""
 
 
