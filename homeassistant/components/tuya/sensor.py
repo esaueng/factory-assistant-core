05e558e49386ca68ec909d46c9dbec44c70a1f04
Douglas Krahmer <doug.github@remhark.com>
2024-06-11 09:09:57 -0700

    Add support for Tuya non-standard contact sensors (#115557)
    Co-authored-by: Franck Nijhof <git@frenck.dev>

--- homeassistant/components/tuya/sensor.py
+++ homeassistant/components/tuya/sensor.py
@@ -56,6 +56,14 @@ BATTERY_SENSORS: tuple[TuyaSensorEntityDescription, ...] = (
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
     TuyaSensorEntityDescription(
+        key=DPCode.BATTERY,  # Used by non-standard contact sensor implementations
+        REDACTED_VALUE"battery",
+        native_unit_of_measurement=PERCENTAGE,
+        device_class=SensorDeviceClass.BATTERY,
+        state_class=SensorStateClass.MEASUREMENT,
+        entity_category=EntityCategory.DIAGNOSTIC,
+    ),
+    TuyaSensorEntityDescription(
         key=DPCode.BATTERY_STATE,
         REDACTED_VALUE"battery_state",
         entity_category=EntityCategory.DIAGNOSTIC,
