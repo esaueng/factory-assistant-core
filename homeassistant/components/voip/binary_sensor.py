54f2ac8030c58b6b1bebd571b68c859810d849df
Erik Montnemery <erik@montnemery.com>
2024-09-12 20:34:11 +0200

    Disable voip call_in_progress binary sensor (#125812)

--- homeassistant/components/voip/binary_sensor.py
+++ homeassistant/components/voip/binary_sensor.py
@@ -42,6 +42,7 @@ class VoIPCallInProgress(VoIPEntity, BinarySensorEntity):
     """Entity to represent voip call is in progress."""
 
     entity_description = BinarySensorEntityDescription(
+        entity_registry_enabled_default=False,
         key="call_in_progress",
         REDACTED_VALUE"call_in_progress",
     )
