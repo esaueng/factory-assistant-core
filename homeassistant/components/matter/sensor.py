c013ef300ce31837e953d907d4e859d09b82be96
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-09-20 12:07:38 +0200

    Mark current position sensor for Matter switch as default disabled (#126254)

--- homeassistant/components/matter/sensor.py
+++ homeassistant/components/matter/sensor.py
@@ -480,6 +480,7 @@ DISCOVERY_SCHEMAS = [
             state_class=SensorStateClass.MEASUREMENT,
             REDACTED_VALUE"switch_current_position",
             entity_category=EntityCategory.DIAGNOSTIC,
+            entity_registry_enabled_default=False,
         ),
         entity_class=MatterSensor,
         required_attributes=(clusters.Switch.Attributes.CurrentPosition,),
