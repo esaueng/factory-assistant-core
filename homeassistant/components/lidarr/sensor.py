e97bb421888ba33504878b8d305958315e32fd2a
Isaac <55418526+iz4c@users.noreply.github.com>
2024-10-25 17:13:03 +0100

    Add "Albums" sensor to Lidarr (#125631)
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/lidarr/sensor.py
+++ homeassistant/components/lidarr/sensor.py
@@ -85,7 +85,7 @@ SENSOR_TYPES: dict[str, LidarrSensorEntityDescription[Any]] = {
     "queue": LidarrSensorEntityDescription[LidarrQueue](
         key="queue",
         REDACTED_VALUE"queue",
-        native_unit_of_measurement="Albums",
+        native_unit_of_measurement="albums",
         value_fn=lambda data, _: data.totalRecords,
         state_class=SensorStateClass.TOTAL,
         attributes_fn=lambda data: {i.title: queue_str(i) for i in data.records},
@@ -93,13 +93,21 @@ SENSOR_TYPES: dict[str, LidarrSensorEntityDescription[Any]] = {
     "wanted": LidarrSensorEntityDescription[LidarrQueue](
         key="wanted",
         REDACTED_VALUE"wanted",
-        native_unit_of_measurement="Albums",
+        native_unit_of_measurement="albums",
         value_fn=lambda data, _: data.totalRecords,
         state_class=SensorStateClass.TOTAL,
         entity_registry_enabled_default=False,
         attributes_fn=lambda data: {
             album.title: album.artist.artistName for album in data.records
         },
+    ),
+    "albums": LidarrSensorEntityDescription[int](
+        key="albums",
+        REDACTED_VALUE"albums",
+        native_unit_of_measurement="albums",
+        value_fn=lambda data, _: data,
+        state_class=SensorStateClass.TOTAL,
+        entity_registry_enabled_default=False,
     ),
 }
 
