6c5154595ec0ee9ca50d1008a9e7e233fa55f3f2
LG-ThinQ-Integration <LG-ThinQ-Integration@lge.com>
2024-10-30 18:34:04 +0900

    Add entity_category to avoid header_toggle for switch (#129477)
    add entity_category to avoid header_toggle
    
    Co-authored-by: yunseon.park <yunseon.park@lge.com>

--- homeassistant/components/lg_thinq/switch.py
+++ homeassistant/components/lg_thinq/switch.py
@@ -38,6 +38,7 @@ DEVICE_TYPE_SWITCH_MAP: dict[DeviceType, tuple[ThinQSwitchEntityDescription, ...
             REDACTED_VALUEThinQProperty.POWER_SAVE_ENABLED,
             on_key="true",
             off_key="false",
+            entity_category=EntityCategory.CONFIG,
         ),
     ),
     DeviceType.AIR_PURIFIER_FAN: (
@@ -111,6 +112,7 @@ DEVICE_TYPE_SWITCH_MAP: dict[DeviceType, tuple[ThinQSwitchEntityDescription, ...
             REDACTED_VALUEThinQProperty.EXPRESS_MODE,
             on_key="true",
             off_key="false",
+            entity_category=EntityCategory.CONFIG,
         ),
         ThinQSwitchEntityDescription(
             key=ThinQProperty.RAPID_FREEZE,
@@ -126,6 +128,7 @@ DEVICE_TYPE_SWITCH_MAP: dict[DeviceType, tuple[ThinQSwitchEntityDescription, ...
             REDACTED_VALUEThinQProperty.HOT_WATER_MODE,
             on_key="on",
             off_key="off",
+            entity_category=EntityCategory.CONFIG,
         ),
     ),
     DeviceType.WINE_CELLAR: (
@@ -134,6 +137,7 @@ DEVICE_TYPE_SWITCH_MAP: dict[DeviceType, tuple[ThinQSwitchEntityDescription, ...
             REDACTED_VALUEThinQProperty.OPTIMAL_HUMIDITY,
             on_key="on",
             off_key="off",
+            entity_category=EntityCategory.CONFIG,
         ),
     ),
 }
