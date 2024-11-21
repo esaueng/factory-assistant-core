524d41c17e48aedce745b2398b6dde295110ca70
G Johansson <goran.johansson@shiftit.se>
2024-11-21 08:31:50 +0100

    Add created sensor in filesize (#131108)

--- homeassistant/components/filesize/sensor.py
+++ homeassistant/components/filesize/sensor.py
@@ -47,6 +47,13 @@ SENSOR_TYPES = (
         device_class=SensorDeviceClass.TIMESTAMP,
         entity_category=EntityCategory.DIAGNOSTIC,
     ),
+    SensorEntityDescription(
+        key="created",
+        REDACTED_VALUE"created",
+        entity_registry_enabled_default=False,
+        device_class=SensorDeviceClass.TIMESTAMP,
+        entity_category=EntityCategory.DIAGNOSTIC,
+    ),
 )
 
 
