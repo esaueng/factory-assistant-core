47cbc93cc3e19351454156a55c9f7d841dcb2be3
Markus Adrario <Mozilla@adrario.de>
2025-02-01 21:11:53 +0100

    Small additions for Homee (#137000)
    * fix entity set value error handling
    
    * Translation for node_state sensor
    
    * add entrance gate operator to covers
    
    * fix review comments
    
    * Update tests/components/homee/test_cover.py
    
    * Delete Logging statement
    
    ---------
    
    Co-authored-by: Joost Lekkerkerker <joostlek@outlook.com>

--- homeassistant/components/homee/sensor.py
+++ homeassistant/components/homee/sensor.py
@@ -177,6 +177,7 @@ SENSOR_DESCRIPTIONS: dict[AttributeType, HomeeSensorEntityDescription] = {
     AttributeType.TOTAL_CURRENT: HomeeSensorEntityDescription(
         key="total_current",
         device_class=SensorDeviceClass.CURRENT,
+        state_class=SensorStateClass.MEASUREMENT,
     ),
     AttributeType.TOTAL_CURRENT_ENERGY_USE: HomeeSensorEntityDescription(
         key="total_power",
@@ -252,7 +253,7 @@ NODE_SENSOR_DESCRIPTIONS: tuple[HomeeNodeSensorEntityDescription, ...] = (
         ],
         entity_category=EntityCategory.DIAGNOSTIC,
         entity_registry_enabled_default=False,
-        REDACTED_VALUE"node_sensor_state",
+        REDACTED_VALUE"node_state",
         value_fn=lambda node: get_name_for_enum(NodeState, node.state),
     ),
 )
