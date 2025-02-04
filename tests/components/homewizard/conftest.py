560ff74c6da49913eb4d5a877c2ad78a842cda35
Duco Sebel <74970928+DCSBL@users.noreply.github.com>
2025-02-04 12:47:50 +0100

    Remove v2 API support for HomeWizard P1 Meter (#137261)

--- tests/components/homewizard/conftest.py
+++ tests/components/homewizard/conftest.py
@@ -160,7 +160,7 @@ def mock_config_entry_v2() -> MockConfigEntry:
             CONF_IP_ADDRESS: "127.0.0.1",
             CONF_TOKEN: "REDACTED_VALUE",
         },
-        unique_id="HWE-P1_5c2fafabcdef",
+        unique_id="HWE-BAT_5c2fafabcdef",
     )
 
 
