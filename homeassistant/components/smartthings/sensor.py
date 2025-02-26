87c7f377a63ca9313723fe8cf43fc8a8d95dfcfb
Joost Lekkerkerker <joostlek@outlook.com>
2025-02-26 16:18:59 +0100

    Set options for alarm sensor in SmartThings (#139345)
    * Set options for alarm sensor in SmartThings
    
    * Set options for alarm sensor in SmartThings
    
    * Fix

--- homeassistant/components/smartthings/sensor.py
+++ homeassistant/components/smartthings/sensor.py
@@ -112,6 +112,8 @@ CAPABILITY_TO_SENSORS: dict[
             SmartThingsSensorEntityDescription(
                 key=Attribute.ALARM,
                 REDACTED_VALUE"alarm",
+                options=["both", "strobe", "siren", "off"],
+                device_class=SensorDeviceClass.ENUM,
             )
         ]
     },
