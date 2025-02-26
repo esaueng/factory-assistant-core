b44d928ff23e0d6d85232adcc64ddc8f126185ff
Joost Lekkerkerker <joostlek@outlook.com>
2025-02-26 17:52:57 +0100

    Add translatable states to smoke detector in SmartThings (#139365)

--- homeassistant/components/smartthings/sensor.py
+++ homeassistant/components/smartthings/sensor.py
@@ -684,6 +684,8 @@ CAPABILITY_TO_SENSORS: dict[
             SmartThingsSensorEntityDescription(
                 key=Attribute.SMOKE,
                 REDACTED_VALUE"smoke_detector",
+                options=["detected", "clear", "tested"],
+                device_class=SensorDeviceClass.ENUM,
             )
         ]
     },
