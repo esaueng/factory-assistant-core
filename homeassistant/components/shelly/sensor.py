c8b62eb0809aff0ff3bed5422d41ae523a30a484
Simone Chemelli <simone.chemelli@gmail.com>
2025-03-18 16:29:21 +0100

    Deprecate Shelly state attributes (#140791)

--- homeassistant/components/shelly/sensor.py
+++ homeassistant/components/shelly/sensor.py
@@ -358,6 +358,7 @@ SENSORS: dict[tuple[str, str], BlockSensorDescription] = {
         REDACTED_VALUE"lamp_life",
         value=get_shelly_air_lamp_life,
         suggested_display_precision=1,
+        # Deprecated, remove in 2025.10
         extra_state_attributes=lambda block: {
             "Operational hours": round(cast(int, block.totalWorkTime) / 3600, 1)
         },
@@ -378,6 +379,7 @@ SENSORS: dict[tuple[str, str], BlockSensorDescription] = {
         options=["warmup", "normal", "fault"],
         REDACTED_VALUE"operation",
         value=lambda value: None if value == "unknown" else value,
+        # Deprecated, remove in 2025.10
         extra_state_attributes=lambda block: {"self_test": block.selfTest},
     ),
     ("valve", "valve"): BlockSensorDescription(
