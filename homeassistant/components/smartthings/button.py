18c096687363b823563529f905506d77caba5ae9
Joost Lekkerkerker <joostlek@outlook.com>
2025-03-26 17:48:03 +0100

    Add button to reset the water filter in SmartThings (#141493)
    * Add button to reset the water filter in SmartThings
    
    * Add button to reset the water filter in SmartThings

--- homeassistant/components/smartthings/button.py
+++ homeassistant/components/smartthings/button.py
@@ -29,6 +29,11 @@ CAPABILITIES_TO_BUTTONS: dict[Capability | str, SmartThingsButtonDescription] = 
         REDACTED_VALUE"stop",
         command=Command.STOP,
     ),
+    Capability.CUSTOM_WATER_FILTER: SmartThingsButtonDescription(
+        key=Capability.CUSTOM_WATER_FILTER,
+        REDACTED_VALUE"reset_water_filter",
+        command=Command.RESET_WATER_FILTER,
+    ),
 }
 
 
