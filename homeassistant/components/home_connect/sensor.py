7600510a6fa17e6f1ba48ef6aff838300b56bcdc
J. Diego Rodríguez Royo <jdrr1998@hotmail.com>
2025-05-01 09:57:30 +0200

    Remove translation key for battery level in Home Connect sensor (#144020)

--- homeassistant/components/home_connect/sensor.py
+++ homeassistant/components/home_connect/sensor.py
@@ -159,7 +159,6 @@ SENSORS = (
     HomeConnectSensorEntityDescription(
         key=StatusKey.BSH_COMMON_BATTERY_LEVEL,
         device_class=SensorDeviceClass.BATTERY,
-        REDACTED_VALUE"battery_level",
     ),
     HomeConnectSensorEntityDescription(
         key=StatusKey.BSH_COMMON_VIDEO_CAMERA_STATE,
