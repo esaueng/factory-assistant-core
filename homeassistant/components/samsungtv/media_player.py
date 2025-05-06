b645c1a0d46427dbee0c73b7617636262d314b3d
Simone Chemelli <simone.chemelli@gmail.com>
2025-05-06 13:20:04 +0300

    Manage unsupported sources on Samsung TV (#144221)

--- homeassistant/components/samsungtv/media_player.py
+++ homeassistant/components/samsungtv/media_player.py
@@ -386,4 +386,8 @@ class SamsungTVDevice(SamsungTVEntity, MediaPlayerEntity):
             await self._async_send_keys([SOURCES[source]])
             return
 
-        LOGGER.error("Unsupported source")
+        raise HomeAssistantError(
+            translation_domain=DOMAIN,
+            REDACTED_VALUE"source_unsupported",
+            translation_placeholders={"entity": self.entity_id, "source": source},
+        )
