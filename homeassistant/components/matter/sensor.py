c62598b6934595a9592138f927cb3d2ae4d3bcb0
jvmahon <jvm33@columbia.edu>
2025-05-22 06:42:05 -0400

    Add matter attributes (#140843)
    * Add Matter attributes
    
    * Add Matter attributes
    
    * Add Matter attributes
    
    * Add Matter attributes
    
    * Update strings.json
    
    * Update homeassistant/components/matter/select.py
    
    Co-authored-by: Marcel van der Veldt <m.vanderveldt@outlook.com>
    
    * Update select.py
    
    Deleted items to be added as switch entities instead.
    
    * Update strings.json
    
    * Update select.py
    
    * Update strings.json
    
    * Fix
    
    * Update strings.json
    
    * Update strings.json
    
    * Fix
    
    * Update select.py
    
    ---------
    
    Co-authored-by: Marcel van der Veldt <m.vanderveldt@outlook.com>
    Co-authored-by: Joostlek <joostlek@outlook.com>

--- homeassistant/components/matter/sensor.py
+++ homeassistant/components/matter/sensor.py
@@ -951,6 +951,42 @@ DISCOVERY_SCHEMAS = [
     MatterDiscoverySchema(
         platform=Platform.SENSOR,
         entity_description=MatterSensorEntityDescription(
+            key="MinPINCodeLength",
+            REDACTED_VALUE"min_pin_code_length",
+            entity_category=EntityCategory.DIAGNOSTIC,
+            device_class=None,
+        ),
+        entity_class=MatterSensor,
+        required_attributes=(clusters.DoorLock.Attributes.MinPINCodeLength,),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterSensorEntityDescription(
+            key="MaxPINCodeLength",
+            REDACTED_VALUE"max_pin_code_length",
+            entity_category=EntityCategory.DIAGNOSTIC,
+            device_class=None,
+        ),
+        entity_class=MatterSensor,
+        required_attributes=(clusters.DoorLock.Attributes.MaxPINCodeLength,),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterSensorEntityDescription(
+            key="TargetPositionLiftPercent100ths",
+            entity_category=EntityCategory.DIAGNOSTIC,
+            REDACTED_VALUE"window_covering_target_position",
+            measurement_to_ha=lambda x: round((10000 - x) / 100),
+            native_unit_of_measurement=PERCENTAGE,
+        ),
+        entity_class=MatterSensor,
+        required_attributes=(
+            clusters.WindowCovering.Attributes.TargetPositionLiftPercent100ths,
+        ),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.SENSOR,
+        entity_description=MatterSensorEntityDescription(
             key="EnergyEvseFaultState",
             REDACTED_VALUE"evse_fault_state",
             device_class=SensorDeviceClass.ENUM,
