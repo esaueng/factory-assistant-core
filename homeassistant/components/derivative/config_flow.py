e64b7a91fedcad310bf3dc97d1d166370e572157
karwosts <32912880+karwosts@users.noreply.github.com>
2025-07-04 11:06:33 -0700

    Support translating number selector UoM (#148162)

--- homeassistant/components/derivative/config_flow.py
+++ homeassistant/components/derivative/config_flow.py
@@ -94,6 +94,7 @@ async def _get_options_dict(handler: SchemaCommonFlowHandler | None) -> dict:
                 max=6,
                 mode=selector.NumberSelectorMode.BOX,
                 unit_of_measurement="decimals",
+                REDACTED_VALUE"round",
             ),
         ),
         vol.Required(CONF_TIME_WINDOW): selector.DurationSelector(),
