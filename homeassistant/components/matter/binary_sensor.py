f31bd9f354566155f7eaa7a3d830041da7e37971
Norbert Rittel <norbert@rittel.de>
2025-07-07 10:26:20 +0200

    Improve translation_key of `EnergyEvseSupplyStateSensor` in `matter` (#148280)

--- homeassistant/components/matter/binary_sensor.py
+++ homeassistant/components/matter/binary_sensor.py
@@ -309,7 +309,7 @@ DISCOVERY_SCHEMAS = [
         platform=Platform.BINARY_SENSOR,
         entity_description=MatterBinarySensorEntityDescription(
             key="EnergyEvseSupplyStateSensor",
-            REDACTED_VALUE"evse_supply_charging_state",
+            REDACTED_VALUE"evse_supply_state",
             device_class=BinarySensorDeviceClass.RUNNING,
             device_to_ha={
                 clusters.EnergyEvse.Enums.SupplyStateEnum.kDisabled: False,
