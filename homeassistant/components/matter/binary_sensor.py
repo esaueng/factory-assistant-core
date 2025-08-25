faf5ad8f28e5a043204cbdb7551349bee5a89bda
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-08-25 15:43:00 +0200

    Matter Valve new attributes (#150788)

--- homeassistant/components/matter/binary_sensor.py
+++ homeassistant/components/matter/binary_sensor.py
@@ -410,6 +410,59 @@ DISCOVERY_SCHEMAS = [
     MatterDiscoverySchema(
         platform=Platform.BINARY_SENSOR,
         entity_description=MatterBinarySensorEntityDescription(
+            key="ValveConfigurationAndControlValveFault_GeneralFault",
+            REDACTED_VALUE"valve_fault_general_fault",
+            device_class=BinarySensorDeviceClass.PROBLEM,
+            entity_category=EntityCategory.DIAGNOSTIC,
+            device_to_ha=lambda x: (
+                x
+                == clusters.ValveConfigurationAndControl.Bitmaps.ValveFaultBitmap.kGeneralFault
+            ),
+        ),
+        entity_class=MatterBinarySensor,
+        required_attributes=(
+            clusters.ValveConfigurationAndControl.Attributes.ValveFault,
+        ),
+        allow_multi=True,
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.BINARY_SENSOR,
+        entity_description=MatterBinarySensorEntityDescription(
+            key="ValveConfigurationAndControlValveFault_Blocked",
+            REDACTED_VALUE"valve_fault_blocked",
+            device_class=BinarySensorDeviceClass.PROBLEM,
+            entity_category=EntityCategory.DIAGNOSTIC,
+            device_to_ha=lambda x: (
+                x
+                == clusters.ValveConfigurationAndControl.Bitmaps.ValveFaultBitmap.kBlocked
+            ),
+        ),
+        entity_class=MatterBinarySensor,
+        required_attributes=(
+            clusters.ValveConfigurationAndControl.Attributes.ValveFault,
+        ),
+        allow_multi=True,
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.BINARY_SENSOR,
+        entity_description=MatterBinarySensorEntityDescription(
+            key="ValveConfigurationAndControlValveFault_Leaking",
+            REDACTED_VALUE"valve_fault_leaking",
+            device_class=BinarySensorDeviceClass.PROBLEM,
+            entity_category=EntityCategory.DIAGNOSTIC,
+            device_to_ha=lambda x: (
+                x
+                == clusters.ValveConfigurationAndControl.Bitmaps.ValveFaultBitmap.kLeaking
+            ),
+        ),
+        entity_class=MatterBinarySensor,
+        required_attributes=(
+            clusters.ValveConfigurationAndControl.Attributes.ValveFault,
+        ),
+    ),
+    MatterDiscoverySchema(
+        platform=Platform.BINARY_SENSOR,
+        entity_description=MatterBinarySensorEntityDescription(
             key="RefrigeratorAlarmDoorOpen",
             REDACTED_VALUE"alarm_door",
             device_class=BinarySensorDeviceClass.PROBLEM,
