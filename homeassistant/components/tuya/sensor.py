e9ceb04bea521c55b50d537364429c9a6b8bfbdb
yufeng <zhangzy@tuya.com>
2025-09-02 19:10:36 +0800

    Add support for new energy sensor entities for TDQ (socket/outlet) devices in the Tuya integration (#151553)

--- homeassistant/components/tuya/sensor.py
+++ homeassistant/components/tuya/sensor.py
@@ -1134,6 +1134,12 @@ SENSORS: dict[str, tuple[TuyaSensorEntityDescription, ...]] = {
             entity_registry_enabled_default=False,
         ),
         TuyaSensorEntityDescription(
+            key=DPCode.ADD_ELE,
+            REDACTED_VALUE"total_energy",
+            device_class=SensorDeviceClass.ENERGY,
+            state_class=SensorStateClass.TOTAL_INCREASING,
+        ),
+        TuyaSensorEntityDescription(
             key=DPCode.VA_TEMPERATURE,
             REDACTED_VALUE"temperature",
             device_class=SensorDeviceClass.TEMPERATURE,
