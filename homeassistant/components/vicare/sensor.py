c60d1e3dc26655bfc4f41bb52b77b5c0bc7bd2be
Christopher Fenner <9592452+CFenner@users.noreply.github.com>
2025-10-05 18:34:45 +0200

    Add sensors for battery charge amount to ViCare integration (#153631)
    Co-authored-by: Josef Zweck <josef@zweck.dev>

--- homeassistant/components/vicare/sensor.py
+++ homeassistant/components/vicare/sensor.py
@@ -721,6 +721,13 @@ GLOBAL_SENSORS: tuple[ViCareSensorEntityDescription, ...] = (
         value_getter=lambda api: api.getElectricalEnergySystemOperationState(),
     ),
     ViCareSensorEntityDescription(
+        key="ess_charge_total",
+        REDACTED_VALUE"ess_charge_total",
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        value_getter=lambda api: api.getElectricalEnergySystemTransferChargeCumulatedLifeCycle(),
+        unit_getter=lambda api: api.getElectricalEnergySystemTransferChargeCumulatedUnit(),
+    ),
+    ViCareSensorEntityDescription(
         key="ess_discharge_today",
         REDACTED_VALUE"ess_discharge_today",
         state_class=SensorStateClass.TOTAL_INCREASING,
