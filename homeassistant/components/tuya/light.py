35278bcccff07f39acf304279a0d2c421c082b14
Heindrich Paul <heindrich.paul@gmail.com>
2025-11-10 13:57:54 +0100

    Added light support to cat litter boxes (#156051)

--- homeassistant/components/tuya/light.py
+++ homeassistant/components/tuya/light.py
@@ -240,6 +240,13 @@ LIGHTS: dict[DeviceCategory, tuple[TuyaLightEntityDescription, ...]] = {
             color_data=DPCode.COLOUR_DATA,
         ),
     ),
+    DeviceCategory.MSP: (
+        TuyaLightEntityDescription(
+            key=DPCode.LIGHT,
+            REDACTED_VALUE"light",
+            entity_category=EntityCategory.CONFIG,
+        ),
+    ),
     DeviceCategory.QJDCZ: (
         TuyaLightEntityDescription(
             key=DPCode.SWITCH_LED,
