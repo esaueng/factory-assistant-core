7ac2c8b6e045ad6cab35af0cd60b173746821fc4
Jaap Pieroen <github@pieroen.nl>
2025-11-27 12:24:07 +0100

    Bugfix: Essent remove average gas price today (#157317)

--- homeassistant/components/essent/sensor.py
+++ homeassistant/components/essent/sensor.py
@@ -102,6 +102,7 @@ SENSORS: tuple[EssentSensorEntityDescription, ...] = (
         key="average_today",
         REDACTED_VALUE"average_today",
         value_fn=lambda energy_data: energy_data.avg_price,
+        energy_types=(EnergyType.ELECTRICITY,),
     ),
     EssentSensorEntityDescription(
         key="lowest_price_today",
