02f5529f53738621237128fa7eb72d083ab8892b
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2025-12-05 08:21:48 +0100

    Set PARALLEL_UPDATES in fressnapf_tracker (#158008)

--- homeassistant/components/fressnapf_tracker/light.py
+++ homeassistant/components/fressnapf_tracker/light.py
@@ -17,6 +17,8 @@ from . import FressnapfTrackerConfigEntry
 from .const import DOMAIN
 from .entity import FressnapfTrackerEntity
 
+PARALLEL_UPDATES = 1
+
 LIGHT_ENTITY_DESCRIPTION = LightEntityDescription(
     REDACTED_VALUE"led",
     entity_category=EntityCategory.CONFIG,
