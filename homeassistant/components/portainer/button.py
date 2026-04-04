ea97f5617595d72614b13b6d37f63a3a390320e0
Erwin Douna <e.douna@gmail.com>
2026-04-04 15:16:39 +0200

    Add kill button to Portainer (#167277)
    Co-authored-by: Joostlek <joostlek@outlook.com>

--- homeassistant/components/portainer/button.py
+++ homeassistant/components/portainer/button.py
@@ -102,6 +102,16 @@ CONTAINER_BUTTONS: tuple[PortainerButtonDescription, ...] = (
             )
         ),
     ),
+    PortainerButtonDescription(
+        key="kill",
+        REDACTED_VALUE"kill_container",
+        entity_category=EntityCategory.CONFIG,
+        press_action=(
+            lambda portainer, endpoint_id, container_id: portainer.kill_container(
+                endpoint_id, container_id
+            )
+        ),
+    ),
 )
 
 
