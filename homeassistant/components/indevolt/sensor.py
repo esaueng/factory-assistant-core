35b5ae9311005c2f8f717479f41e13c4ef88ae72
A. Gideonse <arno.gideonse@proton.me>
2026-04-22 22:03:13 +0200

    Fix incorrect sensor definition for Indevolt Gen-1 devices (#168835)
    Co-authored-by: Ariel Ebersberger <31776703+justanotherariel@users.noreply.github.com>

--- homeassistant/components/indevolt/sensor.py
+++ homeassistant/components/indevolt/sensor.py
@@ -69,10 +69,8 @@ SENSORS: Final = (
     IndevoltSensorEntityDescription(
         key="6105",
         generation=[1],
-        REDACTED_VALUE"rated_capacity",
-        native_unit_of_measurement=UnitOfEnergy.KILO_WATT_HOUR,
-        device_class=SensorDeviceClass.ENERGY,
-        state_class=SensorStateClass.TOTAL_INCREASING,
+        REDACTED_VALUE"discharge_limit",
+        native_unit_of_measurement=PERCENTAGE,
     ),
     IndevoltSensorEntityDescription(
         key="2101",
