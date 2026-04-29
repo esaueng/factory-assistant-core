1f81ec3a96964366f47f8e17ad7b6cd15ccc22bf
Simone Chemelli <simone.chemelli@gmail.com>
2026-04-29 10:12:18 +0200

    Use defaults for device class UPTIME in Shelly (#169148)

--- homeassistant/components/shelly/sensor.py
+++ homeassistant/components/shelly/sensor.py
@@ -467,7 +467,6 @@ REST_SENSORS: Final = {
     ),
     "uptime": RestSensorDescription(
         key="uptime",
-        REDACTED_VALUE"last_restart",
         value=lambda status, _: utcnow() - timedelta(seconds=status["uptime"]),
         device_class=SensorDeviceClass.UPTIME,
         entity_registry_enabled_default=False,
@@ -1243,7 +1242,6 @@ RPC_SENSORS: Final = {
     "uptime": RpcSensorDescription(
         key="sys",
         sub_key="uptime",
-        REDACTED_VALUE"last_restart",
         device_class=SensorDeviceClass.UPTIME,
         value=lambda status, _: utcnow() - timedelta(seconds=status),
         entity_registry_enabled_default=False,
