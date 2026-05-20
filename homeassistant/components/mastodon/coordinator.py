23a9364ff1019100844ab53cc1dc9dafb10b8b48
Andrew Jackson <andrew@codechimp.org>
2026-05-20 14:33:01 +0100

    Add translations to Mastodon exceptions (#171528)

--- homeassistant/components/mastodon/coordinator.py
+++ homeassistant/components/mastodon/coordinator.py
@@ -62,6 +62,9 @@ class MastodonCoordinator(DataUpdateCoordinator[Account]):
                 REDACTED_VALUE"auth_failed",
             ) from error
         except MastodonError as ex:
-            raise UpdateFailed(ex) from ex
+            raise UpdateFailed(
+                translation_domain=DOMAIN,
+                REDACTED_VALUE"update_failed",
+            ) from ex
 
         return account
