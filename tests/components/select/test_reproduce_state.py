be9c888a9926094dc151a44f37cd42237e0dfd9e
epenet <6771947+epenet@users.noreply.github.com>
2026-05-29 13:50:34 +0000

    Reduce

--- tests/components/select/test_reproduce_state.py
+++ tests/components/select/test_reproduce_state.py
@@ -3,10 +3,10 @@
 import pytest
 
 from homeassistant.components.select.const import (
-    ATTR_OPTION,
-    ATTR_OPTIONS,
     DOMAIN,
     SERVICE_SELECT_OPTION,
+    SelectEntityAttribute,
+    SelectServiceArgument,
 )
 from homeassistant.const import ATTR_ENTITY_ID
 from homeassistant.core import HomeAssistant, State
@@ -23,7 +23,7 @@ async def test_reproducing_states(
     hass.states.async_set(
         "select.test",
         "option_one",
-        {ATTR_OPTIONS: ["option_one", "option_two", "option_three"]},
+        {SelectEntityAttribute.OPTIONS: ["option_one", "option_two", "option_three"]},
     )
 
     await async_reproduce_state(
@@ -35,7 +35,10 @@ async def test_reproducing_states(
 
     assert len(calls) == 1
     assert calls[0].domain == DOMAIN
-    assert calls[0].data == {ATTR_ENTITY_ID: "select.test", ATTR_OPTION: "option_two"}
+    assert calls[0].data == {
+        ATTR_ENTITY_ID: "select.test",
+        SelectServiceArgument.OPTION: "option_two",
+    }
 
     # Calling it again should not do anything
     await async_reproduce_state(
