74ee4ff14983fcb49c34f982112c0f4f24533cf6
Peter <171875562+petergstfsn@users.noreply.github.com>
2026-06-14 20:19:04 -0400

    update

--- homeassistant/components/onboarding/const.py
+++ homeassistant/components/onboarding/const.py
@@ -16,8 +16,15 @@ STEP_USER = "user"
 STEP_CORE_CONFIG = "core_config"
 STEP_INTEGRATION = "integration"
 STEP_ANALYTICS = "analytics"
+STEP_FACTORY_ASSISTANT_INDUSTRIAL = "factory_assistant_industrial"
 
-STEPS = [STEP_USER, STEP_CORE_CONFIG, STEP_ANALYTICS, STEP_INTEGRATION]
+STEPS = [
+    STEP_USER,
+    STEP_CORE_CONFIG,
+    STEP_FACTORY_ASSISTANT_INDUSTRIAL,
+    STEP_ANALYTICS,
+    STEP_INTEGRATION,
+]
 
 DEFAULT_AREAS = (
     DefaultArea(key="living_room", icon="mdi:sofa"),
